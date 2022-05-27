d10812ff9a51a919e3fb820e0e0bda53e22872c4
Yevhen Podoliaka <zanp85740@gmail.com>
2022-05-27 08:32:23 +0300

    create task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    btnStart  : document.querySelector('[data-start]'),
+    btnStop : document.querySelector('[data-stop]'),
+}
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+let timerId = null;
+refs.btnStop.disabled = true;
 
+function onBtnStartClick() {
+    changeBgc();
+    refs.btnStop.disabled = false;
+    refs.btnStart.disabled = true;
+ };
+function onBtnStopClick() { 
+    clearInterval(timerId);
+       refs.btnStart.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeBgc() {
+    timerId = setInterval(
+      () => (document.body.style.backgroundColor = getRandomHexColor()),
+      1000,
+    );
+
+  ;  
+}
